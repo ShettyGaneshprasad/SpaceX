83439f78d148f17e75c565deb507c7b195bae6fe
ganeshshetty98 <gs00619@gmail.com>
2020-06-18 12:53:58 +0530

    Create web_view_container.dart

---
+++ lib/web_view_container.dart
@@ -0,0 +1,40 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
+import 'connection.dart';
+
+class WebViewContainer extends StatefulWidget {
+  final url;
+  WebViewContainer(this.url);
+  @override
+  createState() => _WebViewContainerState(this.url);
+}
+
+class _WebViewContainerState extends State<WebViewContainer> {
+  var _url;
+  final _key = UniqueKey();
+  _WebViewContainerState(this._url);
+  @override
+  Widget build(BuildContext context) {
+    if (true) {
+      return Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: WebView(
+                key: _key,
+                javascriptMode: JavascriptMode.unrestricted,
+                initialUrl: _url,
+              ),
+            )
+          ],
+        ),
+      );
+    } else {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+  }
+}

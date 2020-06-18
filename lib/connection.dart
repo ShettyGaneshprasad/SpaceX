d901fb2635cbacd18c4e31e2e612eea99fd86394
ganeshshetty98 <gs00619@gmail.com>
2020-06-18 12:53:47 +0530

    Update connection.dart

--- lib/connection.dart
+++ lib/connection.dart
@@ -1,14 +1,1 @@
-import 'package:connectivity/connectivity.dart';
-import 'connection.dart';
 
-class Connectivity {
-  checkConnection() {
-    var connectivityResult = Connectivity().checkConnectivity();
-    if (connectivityResult == ConnectivityResult.mobile ||
-        connectivityResult == ConnectivityResult.wifi)
-      return true;
-    else {
-      return false;
-    }
-  }
-}
